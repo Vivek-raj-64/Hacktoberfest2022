fac7bd523779cb75bd3f0cb60a19ab971a8735f8
Vivek-raj-64 <92172556+Vivek-raj-64@users.noreply.github.com>
2022-10-05 13:25:23 +0530

    added pascal triangle program

---
+++ C++/pascalTriangle.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pascal(int num)
+{
+int fact=1;
+  for(int i=2; i<=num; i++)
+  {
+    fact=fact*i;
+  }
+  return fact;
+}
+
+int main()
+{
+  int n;
+  cout<<"enter number of terms";
+  cin>>n;
+  for(int r=0; r<n; r++)
+  {
+    for(int c=0; c<=r; c++)
+    {
+      cout<<pascal(r)/(pascal(c)*pascal(r-c))<<" ";
+  }
+  cout<<endl;
+}
+}
